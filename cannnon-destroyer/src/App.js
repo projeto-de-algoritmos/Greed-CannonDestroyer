dea2c15867c79a58fc15398ffcb65aaf6e4884d4
Danillo Souza <danillosouza1704@gmail.com>
2021-03-19 15:59:07 -0300

    Added some css and new ships
    Co-authored-By: João Guedes <isudjoao@gmail.com>

--- cannnon-destroyer/src/App.js
+++ cannnon-destroyer/src/App.js
@@ -6,41 +6,52 @@ import {
   SimpleGrid,
   Text,
   HStack,
+  Button,
 } from "@chakra-ui/react";
 import ChooseQuantity from "./component/ChooseQuantity";
-import Knapsack from "./utils/knapsack";
+import Knapsack from "./utils/Knapsack";
 import cannon from "./assets/cannon.png";
-import ship from "./assets/ship.png";
+import ship1 from "./assets/ship1.png";
+import ship2 from "./assets/ship2.png";
+import ship3 from "./assets/ship3.png";
+import ship1Wrecked from "./assets/ship1Wrecked.png";
+import ship2Wrecked from "./assets/ship2Wrecked.png";
+import ship3Wrecked from "./assets/ship3Wrecked.png";
 import { useState } from "react";
 
 function App() {
+  const knapsack = new Knapsack();
   const [ball1, setBall1] = useState(5);
   const [ball2, setBall2] = useState(5);
   const [ball3, setBall3] = useState(5);
-  const knapsack = new Knapsack();
-  console.log(knapsack);
 
   return (
-    <div>
+    <>
       <Flex bgGradient="radial(gray.300,yellow.300,blue.400)" height="100vh">
-        <Center>
-          <VStack>
-            <img src={cannon} />
+        <HStack>
+          <VStack mt="10%" spacing={10}>
             <VStack>
-              <SimpleGrid columns={3} spacing={10}>
-                <Text>Força</Text>
-                <Text>Qntd</Text>
-                <Text>Peso</Text>
-              </SimpleGrid>
+              <img src={cannon} />
+              <HStack spacing={6}>
+                <Text fontWeight="bold" fontSize="xl">
+                  Força
+                </Text>
+                <Text fontWeight="bold" fontSize="xl">
+                  Quantidade
+                </Text>
+                <Text fontWeight="bold" fontSize="xl">
+                  Peso
+                </Text>
+              </HStack>
               <SimpleGrid columns={3}>
                 <VStack>
-                  <Center pl="50px" h="40px" w="50px">
+                  <Center pl="30px" h="40px" w="50px">
                     <Text>50</Text>
                   </Center>
-                  <Center pl="50px" h="40px" w="50px">
+                  <Center pl="30px" h="40px" w="50px">
                     <Text>50</Text>
                   </Center>
-                  <Center pl="50px" h="40px" w="50px">
+                  <Center pl="30px" h="40px" w="50px">
                     <Text>50</Text>
                   </Center>
                 </VStack>
@@ -50,32 +61,55 @@ function App() {
                   <ChooseQuantity value={ball3} setValue={setBall3} />
                 </VStack>
                 <VStack>
-                  <Text>10</Text>
-                  <Text>10</Text>
-                  <Text>10</Text>
+                  <Center pr="20px" h="40px" w="50px">
+                    <Text>50</Text>
+                  </Center>
+                  <Center pr="20px" h="40px" w="50px">
+                    <Text>50</Text>
+                  </Center>
+                  <Center pr="20px" h="40px" w="50px">
+                    <Text>50</Text>
+                  </Center>
                 </VStack>
               </SimpleGrid>
-              <button onClick={() => console.log(knapsack.getAnswer())}>
-                {" "}
-                Clique em mim
-              </button>
             </VStack>
+            <HStack>
+              <Button
+                bg="green.500"
+                color="white"
+                onClick={() => console.log(knapsack.getAnswer())}
+                borderRadius="20px"
+                size="lg"
+              >
+                LANÇAR
+              </Button>
+              <Button
+                bg="red.500"
+                color="white"
+                onClick={() => console.log(knapsack.getAnswer())}
+                borderRadius="20px"
+                size="lg"
+              >
+                RESETAR
+              </Button>
+            </HStack>
           </VStack>
-
-          <Box
+          <Center
             d="flex"
             flexDirection="row-reverse"
-            flex="3"
+            flex="1"
             height="70vh"
             margin="1rem"
-            bgGradient="linear(to-t, blue.500,green.200,)"
+            bgGradient="linear(to-t, blue.500,green.300,)"
             borderRadius="10px"
           >
-            <img src={ship} height="40%" width="40%" />
-          </Box>
-        </Center>
+            <img src={ship3} height="20%" width="20%" />
+            <img src={ship2} height="30%" width="30%" />
+            <img src={ship1} height="40%" width="40%" />
+          </Center>
+        </HStack>
       </Flex>
-    </div>
+    </>
   );
 }
 
--- cannnon-destroyer/src/component/ChooseQuantity.js
+++ cannnon-destroyer/src/component/ChooseQuantity.js
@@ -9,10 +9,10 @@ function ChooseQuantity({value, setValue}) {
   };
 
   return (
-    <HStack maxW="200px">
-      <Button onClick={() => inc()}>-</Button>
-      <Text>{value}</Text>
-      <Button onClick={() => dec()}>+</Button>
+    <HStack maxW="200px" bg="gray.300" borderRadius="20px">
+      <Button onClick={() => inc()} borderRadius="20px 0px 0px 20px" bg="gray.400">-</Button>
+      <Text>{value===10?'': "0"}{value}</Text>
+      <Button onClick={() => dec()} borderRadius="0px 20px 20px 0px"bg="gray.400">+</Button>
     </HStack>
   );
 }
